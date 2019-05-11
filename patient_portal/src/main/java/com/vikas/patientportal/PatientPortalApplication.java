2ae42986ec6872c1b2dfda46eff680901ee7a72b
Vikas175Prasad <iamvikasandc@gmail.com>
2019-05-11 13:57:54 +0530

    First commit

--- patient_portal/src/main/java/com/vikas/patientportal/PatientPortalApplication.java
+++ patient_portal/src/main/java/com/vikas/patientportal/PatientPortalApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.vikas.patientportal","com.vikas.patientportal.controllers"})
 public class PatientPortalApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientPortalApplication.class, args);
 	}
